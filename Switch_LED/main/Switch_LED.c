b9eabbea170458982908fbea5407cba72248805c
Kunagkone <reenfurrymasor@gmail.com>
2025-09-18 08:13:07 +0700

    commit

--- Switch_LED/main/Switch_LED.c
+++ Switch_LED/main/Switch_LED.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <driver/gpio.h>
-#include <freeRTOS/freeRTOS.h>
-#include <freeRTOS/Task.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 
 void app_main(void)
 {
@@ -13,22 +13,26 @@ void app_main(void)
     int LED3_status = 0;
 
     // 1. gpio_set_direction pin 16 and 18 to Output.
-    gpio_set_direction(16, GPIO_MODE_OUTPUT);
+    gpio_set_direction(16, GPIO_MODE_OUTPUT);  // LED1
+    gpio_set_direction(18, GPIO_MODE_OUTPUT);  // LED3
 
     // 2. gpio_set_direction pin 15 and 17 to Input.
-    gpio_set_direction(15, GPIO_MODE_INPUT);
+    gpio_set_direction(15, GPIO_MODE_INPUT);  // SW1
+    gpio_set_direction(17, GPIO_MODE_INPUT);  // SW2
 
- 
     while(1)
     {
         // read level of gpio 15 and store in SW1_Status 
-        SW1_Status =  gpio_get_level(15);
-
+        SW1_Status = gpio_get_level(15);
         LED1_status = !SW1_Status;     
-
         gpio_set_level(16, LED1_status);
-   
-         //   Delay
+
+        // read level of gpio 17 and store in SW2_Status 
+        SW2_Status = gpio_get_level(17);
+        LED3_status = !SW2_Status;
+        gpio_set_level(18, LED3_status);
+
+        // Delay
         vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
